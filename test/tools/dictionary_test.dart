8be72297ce872624571b8b5b07b67639d51ace71
Benno Richters <b.richters@gmail.com>
2018-10-28 12:00:38 +0100

    replaced testing code with a todo

--- test/tools/dictionary_test.dart
+++ test/tools/dictionary_test.dart
@@ -2,13 +2,7 @@ import 'package:test/test.dart';
 
 void main() {
   test('SuffixCondition', () {
-    RegExp r = RegExp("[^ab]c\$");
-
-
-    print(r.hasMatch("testac"));
-    print(r.hasMatch("testbc"));
-    print(r.hasMatch("testcc"));
-    print(r.hasMatch("testccd"));
+    // TODO
 
   });
 }
