4b756e9932cab4f56aa751939fb27dca228fd5d3
Benno Richters <b.richters@gmail.com>
2018-11-28 08:41:59 +0100

    finished unit test for toggle settings

--- test/unit/screens/game/settings/widgets/toggle_setting_test.dart
+++ test/unit/screens/game/settings/widgets/toggle_setting_test.dart
@@ -4,20 +4,48 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('find icon', (WidgetTester tester) async {
-    ValueNotifier<bool> v = ValueNotifier(true);
+    ValueNotifier<bool> v = ValueNotifier(false);
     var list = ToggleSetting.create(v, Icons.ac_unit);
-    await tester.pumpWidget(wrap(child: list[0]));
+    await tester.pumpWidget(testable(children: list));
 
     var icon = find.byIcon(Icons.ac_unit);
-
     expect(icon, findsOneWidget);
+  });
+
+  testWidgets('toggle switch', (WidgetTester tester) async {
+    ValueNotifier<bool> v = ValueNotifier(false);
+    var list = ToggleSetting.create(v, Icons.ac_unit);
+    await tester.pumpWidget(testable(children: list));
+
+    var toggleWidget = find.byType(Switch);
+    expect(toggleWidget, findsOneWidget);
+
+    expect(v.value, false);
+
+    await tester.tap(toggleWidget);
+    await tester.pump();
+    expect(v.value, true);
+
+    await tester.tap(toggleWidget);
+    await tester.pump();
+    expect(v.value, false);
+
+    await tester.tap(toggleWidget);
+    await tester.pump();
+    expect(v.value, true);
+
+    await tester.tap(toggleWidget);
+    await tester.pump();
+    expect(v.value, false);
   });
 }
 
-Widget wrap({ Widget child }) {
+Widget testable({List<Widget> children}) {
   return MaterialApp(
-    home: Center(
-      child: Material(child: child),
+    home: Material(
+      child: Row(
+        children: children,
+      ),
     ),
   );
 }
