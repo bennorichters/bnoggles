ebfcc5821388c606632f9867e2ecbc5d4ef682ae
Benno Richters <b.richters@gmail.com>
2018-12-15 13:46:59 +0100

    DartDoc

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -8,7 +8,13 @@ import 'package:bnoggles/utils/preferences.dart';
 import 'package:bnoggles/widgets/start_game_button.dart';
 import 'package:flutter/material.dart';
 
+/// Screen where the user can changes the settings.
+///
+/// Settings are stored in an instance of [Preferences].
 class SettingsScreen extends StatelessWidget {
+  /// Creates a [SettingsScreen].
+  ///
+  /// The passed in [preferences] hold the initial settings.s
   SettingsScreen({Key key, @required this.preferences}) : super(key: key);
 
   final Preferences preferences;
