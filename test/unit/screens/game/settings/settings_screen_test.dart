00d3b1919ca1d4e1555c6eaf80c28057950734b5
Benno Richters <b.richters@gmail.com>
2018-12-15 13:33:39 +0100

    moved helper method

--- test/unit/screens/game/settings/settings_screen_test.dart
+++ test/unit/screens/game/settings/settings_screen_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mockito/mockito.dart';
 
-import 'widgets/test_helper.dart';
+import '../../../test_helper.dart';
 
 class MockPreferences extends Mock implements Preferences {}
 
--- test/unit/screens/game/settings/widgets/language_setting_test.dart
+++ test/unit/screens/game/settings/widgets/language_setting_test.dart
@@ -8,7 +8,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'test_helper.dart';
+import '../../../../test_helper.dart';
 
 void main() {
   testWidgets('find icon and label', (WidgetTester tester) async {
