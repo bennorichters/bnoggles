2b8a49cb05e3cffa61717738da80e478afe91a3a
Benno Richters <b.richters@gmail.com>
2018-12-20 18:53:09 +0100

    test for clock

---
+++ test/unit/screens/game/widgets/game_clock_test.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2018, The Bnoggles Team.
+// Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+import 'package:bnoggles/screens/game/widgets/game_clock.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../widget_test_helper.dart';
+
+void main() {
+  testWidgets('clock time and callback when finished', (WidgetTester tester) async {
+    bool finished = false;
+
+    int seconds = 100;
+    void result() {
+      finished = true;
+    }
+
+    var controller = AnimationController(
+      duration: Duration(seconds: seconds),
+      vsync: tester,
+    );
+
+    Clock clock = Clock(result, controller, seconds);
+    await tester.pumpWidget(testableWidget(child: clock));
+
+    expect(find.text('1:41'), findsOneWidget);
+    expect(finished, false);
+
+    controller.value = .5;
+    await tester.pump();
+    expect(find.text('0:51'), findsOneWidget);
+    expect(finished, false);
+
+    controller.value = 1;
+    await tester.pump();
+    expect(find.text('0:00'), findsOneWidget);
+    expect(finished, true);
+  });
+}
