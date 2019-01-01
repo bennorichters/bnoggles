800b8b137e49ecc2835400ae255f2205ac68077e
Benno Richters <b.richters@gmail.com>
2019-01-01 10:16:12 +0100

    Copyright (c) 2019

--- test/unit/utils/gamelogic/coordinate_test.dart
+++ test/unit/utils/gamelogic/coordinate_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2018, The Bnoggles Team.
+// Copyright (c) 2019, The Bnoggles Team.
 // Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
