5a52f0923c34547b9cb4ac12ad442b7bb9424bd4
Benno Richters <b.richters@gmail.com>
2019-01-01 17:12:43 +0100

    reverted had coded test value

--- lib/utils/preferences.dart
+++ lib/utils/preferences.dart
@@ -74,8 +74,7 @@ class Preferences {
   /// Creates [GameParameters] based on these Preferences
   GameParameters toParameters() => GameParameters._(
         languageCode: const ['nl', 'en', 'hu'][language.value],
-//        numberOfPlayers: numberOfPlayers.value,
-        numberOfPlayers: 4,
+        numberOfPlayers: numberOfPlayers.value,
         hasTimeLimit: hasTimeLimit.value,
         time: time.value,
         boardWidth: boardWidth.value,
